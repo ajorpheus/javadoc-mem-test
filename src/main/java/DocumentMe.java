a89f7cd3127c21794cb9935a4a3d7e33fe0feecd
Ashutosh Jindal <Ashutosh.Jindal@monitise.com>
2014-04-17 04:31:09 +0100

    Add maven site and javadoc plugin
    Change-Id: Ibd0c222704692df5fef911f30ae88e34db1e4046

--- src/main/java/DocumentMe.java
+++ src/main/java/DocumentMe.java
@@ -1,2 +1,17 @@
+/**
+ * This class is meant solely for documentation
+ */
 public class DocumentMe {
+
+	private void someMethod(){
+		System.out.println("Heeee");
+	}
+
+	/**
+	 * psvm
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Yay");
+	}
 }
